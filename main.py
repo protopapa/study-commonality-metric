d8fb302c0adb07f14b93f35a2b7a2ed28370d430
Anastasia Protopapa <anastasia@wgtwo.com>
2023-01-25 16:43:14 +0100

    first commit with rankng creation

---
+++ main.py
@@ -0,0 +1,8 @@
+from pprint import pprint
+
+from rankings import build_user_p
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    p = build_user_p()
+    pprint(p)
---
+++ rankings.py
@@ -0,0 +1,17 @@
+import secrets
+
+# TODO Fix the random to make more with mainstream
+editorial_categories = ["female", "nb", "na", "ea", "ma", "sa", "wa", "me", "ca", "sea", "non-binary", "mains"]
+
+
+# Function that creates list/rankings for the given number of users.
+# The purpose of this function is purely for testing the rest of the algorithm.
+def build_user_p():
+    p = []
+    for index in range(20):
+        tags = {secrets.choice(editorial_categories)}
+        track = {"artist": "s" + str(index), "track": "t" + str(index)}
+        pu = {"_id": track, "tags": tags, "i": index}
+        p.append(pu)
+
+    return p
