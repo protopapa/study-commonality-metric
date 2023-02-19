3fe65954c70be4d2a783f27a8da502260d2bfd14
Anastasia Protopapa <anastasia@wgtwo.com>
2023-02-19 22:21:29 +0100

    familiarity for one user given a random ranking

--- commonality.py
+++ commonality.py
@@ -15,14 +15,16 @@ def calculate_user_recall_for_g_in_k(pu, g, k):
     return g_el_k / g_el_pu if g_el_pu else 0
 
 
-# TODO decide how you do the gama.
-def calculate_probability_to_stop_at_rank_i():
-    return 1
+def calculate_rank_biased_precision(gama, k):
+    rbp = (1 - gama) * pow(gama, k - 1)
+    return rbp
 
 
-def calculate_user_familiarity_for_g(pu, g):
+def calculate_user_familiarity_for_g(pu, g, gama):
     familiarity = 0
     for i in range(len(pu)):
-        familiarity += calculate_user_recall_for_g_in_k(pu, g, i)
+        tr = calculate_user_recall_for_g_in_k(pu, g, i)
+        tp = calculate_rank_biased_precision(gama, i)
+        familiarity += tp * tr
 
     return familiarity
--- main.py
+++ main.py
@@ -2,14 +2,25 @@ from pprint import pprint
 
 import numpy as np
 
-from commonality import calculate_user_recall_for_g_in_k
+from commonality import calculate_user_recall_for_g_in_k, calculate_rank_biased_precision, \
+    calculate_user_familiarity_for_g
 from rankings import generate_p_user, editorial_categories
+
+# First run will assume that all users have the same patience,
+# which will be set at start up through a passing argument,
+# or randomly chosen for all users.
+
+user_patience = 0.5
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pu = generate_p_user()
+    pu = generate_p_user(100)
     np.random.shuffle(pu)
 
-    recall = calculate_user_recall_for_g_in_k(pu, editorial_categories[0], 10)
-    # familiarity = calculate_familiarity_u_g(pu, editorial_categories[0])
-    # print(f"familiarity", familiarity)
+    pprint("User List: \n")
+    pprint(pu)
+
+    # recall = calculate_user_recall_for_g_in_k(pu, editorial_categories[0], 20)
+    # rbp = calculate_rank_biased_precision(user_patience, 20)
+    familiarity = calculate_user_familiarity_for_g(pu, editorial_categories[0], user_patience)
+    print(f"familiarity", familiarity)
