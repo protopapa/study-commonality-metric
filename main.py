0b91059ef005d100cbe70399b4c45531773a6b6a
Anastasia Protopapa <anastasia@wgtwo.com>
2023-02-21 21:38:20 +0100

    calc commonality for all categories

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-from pprint import pprint
-
-from commonality import calculate_user_recall_for_g_in_k, calculate_rank_biased_precision, \
-    calculate_user_familiarity_for_g, calculate_commonality_for_g
-from rankings import generate_p_user, editorial_categories, generate_p_users
+from commonality import calculate_user_familiarity_for_g, calculate_commonality_for_g
+from rankings import editorial_categories, generate_p_users
 
 user_patience = 0.8
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    users = generate_p_users(30, 3000)
+    users = generate_p_users(10, 3000)
 
     familiarities = []
-    for pu in users:
-        familiarity = calculate_user_familiarity_for_g(pu, editorial_categories[0], user_patience)
-        familiarities.append(familiarity)
+    commonalities = []
 
-    pprint(familiarities)
+    for g in editorial_categories:
+        fg = []
+        for pu in users:
+            familiarity = calculate_user_familiarity_for_g(pu, g, user_patience)
+            fg.append(familiarity)
+        familiarities.append(fg)
 
-    commonality = calculate_commonality_for_g(familiarities)
-    print(f"Commonality", commonality)
+    for f in familiarities:
+        commonality = calculate_commonality_for_g(f)
+        commonalities.append(commonality)
+    print(f"Commonalities", commonalities)
