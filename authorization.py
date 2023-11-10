f6e66bf041a86375a031451695297b65c7068ebd
Anastasia Protopapa <anastasia@wgtwo.com>
2023-11-10 21:30:01 +0100

    add command line arguments for running the app

--- authorization.py
+++ authorization.py
@@ -2,9 +2,6 @@ import requests
 from requests.auth import HTTPBasicAuth
 from requests_oauthlib import OAuth2Session
 
-client_id = "fa944f548f4b4ef9ba5ec46f1563d590"
-client_secret = "a1c4d42355c54c52ac286330be838045"
-
 # The Spotify API endpoints used by this authorization code
 authorization_base_url = "https://accounts.spotify.com/authorize"
 token_url = "https://accounts.spotify.com/api/token"
@@ -12,7 +9,7 @@ token_url = "https://accounts.spotify.com/api/token"
 scope = ["playlist-modify-public", "user-top-read", "user-read-email", "user-read-private"]
 
 
-def create_spotify_session():
+def create_spotify_session(client_id):
     return OAuth2Session(client_id, scope=scope, redirect_uri='https://localhost:8888/callback')
 
 
@@ -22,7 +19,7 @@ def get_user_authorization(session):
     return authorization_url
 
 
-def get_token(session, rrsp):
+def get_token(session, rrsp, client_id, client_secret):
     auth = HTTPBasicAuth(client_id, client_secret)
     session.fetch_token(token_url, auth=auth, authorization_response=rrsp)
     return session
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import argparse
-from pprint import pprint
 from authorization import *
 from commonality import calculate_user_familiarity_for_g, calculate_commonality_for_g
 from rankings import editorial_categories, generate_p_users
-from playlist import create_playlist
 from user import get_user_info
 
-access_token = ""
-refresh_token = ""
+client_id = ""
+client_secret = ""
 
 
 def users(n, m):
@@ -31,26 +29,29 @@ def metric_calculations(p, user_list):
     print(f"Commonalities", commonalities)
 
 
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser()
-#     # Adding optional argument
 #     parser.add_argument("-m", "--Users", help="Amount of Users to generate")
 #     parser.add_argument("-n", "--List", help="Size of rank list to generate")
 #     parser.add_argument("-p", "--Patience", help="User's patience to use at the RBP")
-#     # Read arguments from command line
-#     args = parser.parse_args()
-#
 #     ul = users(int(args.Users), int(args.List))
 #     pprint(ul)
-#
 #     metric_calculations(float(args.Patience), ul)
 
 
 if __name__ == '__main__':
-    session = create_spotify_session()
+    parser = argparse.ArgumentParser()
+    # Adding optional argument
+    parser.add_argument("-ci", "--ClientId", help="Client Id of the Spotify Application")
+    parser.add_argument("-cs", "--ClientSecret", help="Client Secret of the Spotify Application")
+    # Read arguments from command line
+    args = parser.parse_args()
+
+    client_id = args.ClientId
+    client_secret = args.ClientSecret
+
+    session = create_spotify_session(client_id)
     get_user_authorization(session)
     # Get the authorization verifier code from the callback url
     redirect_response = input('\n\nPaste the full redirect URL here: ')
-    session = get_token(session, redirect_response)
+    session = get_token(session, redirect_response, client_id, client_secret)
 
     get_user_info(session)
